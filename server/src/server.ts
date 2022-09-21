ffc66eaf93ce3a21241865f6942b5b29e644d96d
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2022-09-21 20:08:53 -0300

    feat(root): first class · app base

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.json([
+    { id: 1, name: "name 1" },
+    { id: 2, name: "name 2" },
+    { id: 3, name: "name 3" },
+    { id: 4, name: "name 4" },
+  ]);
+});
+
+app.listen(3000);
